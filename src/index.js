1e3b8329d926cc9543877898fcd3e42427308f10
Harry Wickham <51520004+HarryWickham@users.noreply.github.com>
2023-02-24 13:43:27 +0000

    initial Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const footerMessage = [
+  'Lovingly crafted',
+  'Handmade',
+  'Forged in fire',
+  'Meticulously designed',
+  'Carefully created'
+]
+
+export const ExampleComponent = () => {
+  return (
+    <div>
+      © {new Date().getFullYear()},{' '}
+      {footerMessage[Math.floor(Math.random() * footerMessage.length)]} by{' '}
+      <a href='https://yordevs.com' target='_blank'>
+        Yordevs
+      </a>
+    </div>
+  )
 }
